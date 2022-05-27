95ee4bde364dd04456467ce5c84c4fdd5bb41c13
yoko <kionako96@gmail.com>
2022-05-27 10:43:05 +0900

    0526

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+module.exports = {
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty',
+      };
+    }
+
+    return config;
+  },
   reactStrictMode: true,
 };
-
-module.exports = nextConfig;
